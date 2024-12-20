b27a52d831027653b2c1847c514bde562af41884
adam87328 <adam1dohnal@gmail.com>
2024-12-20 13:48:50 +0100

    basic map works

--- igc_lens/settings.py
+++ igc_lens/settings.py
@@ -131,3 +131,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LEAFLET_CONFIG = {
+    # conf here
+}

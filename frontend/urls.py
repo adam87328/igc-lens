9f4601c73959045db46ff6f2370e9dc6b48b61a0
adam87328 <adam1dohnal@gmail.com>
2025-01-05 20:55:17 +0100

    navbar update

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,8 +4,11 @@ from . import views
 app_name = "frontend"
 urlpatterns = [
     path('', views.HomepageView.as_view(), name='homepage'),
-    path('statistics', views.StatisticsView.as_view(), name='statistics'),
     path('flights/table/', views.FlightTableView.as_view(), name='flights_table'),
     path('flights/map/', views.FlightMapView.as_view(), name='flights_map'),
     path("flightdetail/<int:pk>/", views.FlightDetailView.as_view(), name="flight_detail"),
+    path('stats/totals', views.StatisticsView.as_view(), name='totals'),
+    path('stats/years', views.StatisticsView.as_view(), name='years'),
+    path('stats/places', views.StatisticsView.as_view(), name='places'),
+    path('stats/evolution', views.StatisticsView.as_view(), name='evolution'),
 ]
